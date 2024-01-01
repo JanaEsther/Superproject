ee9a2eb094b0d5fb992801ec73ac572cb808e45a
Jana Obendraufová <j.obendraufova@h2o-logic.cz>
2024-01-01 20:52:44 +0100

    logos_js

--- .eleventy.js
+++ .eleventy.js
@@ -8,9 +8,7 @@ module.exports = function(eleventyConfig) {
 	// Zkopírovat css/ to _site/css/
 	eleventyConfig.addPassthroughCopy("css");
 
-  // Zkopírovat logos/do _site/logos
-  eleventyConfig.addPassthroughCopy("logos");
-
+ 
   return {
     // možné formáty šablon
     templateFormats: ["njk", "html", "md", "liquid"],
